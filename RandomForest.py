e1b8011295a0422bc587ef0c2f30baab97dfb905
DatenBergwerk <spartan2@hotmail.de>
2017-05-17 19:48:18 +0200

    Added logging mechanics, draft for ensemble(ExtraTrees, randomForest, LinearSVC, perhaps NN)

--- RandomForest.py
+++ RandomForest.py
@@ -1,14 +1,15 @@
+import logging
 from math import sqrt
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.svm import LinearSVC
 
-
-np.random.seed(0)
-
-print("Loading data...")
+logging.basicConfig(filename="numerai_logger.log",
+                    level=logging.INFO,
+                    format="%(asctime)s %(levelname)s %(message)s",
+                    datefmt="%d.%m.%Y %H:%M:%S")
+logging.info("Loading Data")
 # Load the data from the CSV files
 training_data = pd.read_csv('numerai_training_data.csv', header=0)
 prediction_data = pd.read_csv('numerai_tournament_data.csv', header=0)
@@ -22,22 +23,32 @@ x_train, y_train = X.iloc[ind[0]], Y.iloc[ind[0]]
 x_test, y_test = X.iloc[ind[1]], Y.iloc[ind[1]]
 tournament = prediction_data[features]
 ids = prediction_data["id"]
+logging.info("Data loading and transformation finished")
 
-print("Training...")
+logging.info("Beginning training")
+
 # Random Forest
+logging.info("Training Start: Model ExtraTrees")
 param_grid = {"max_features": range(int(sqrt(X.shape[1])), int(X.shape[1]*0.5)),
               "n_estimators": [val for sublist in [[10, 25], list(range(50, 501, 50))] for val in sublist]}
-model = GridSearchCV(RandomForestClassifier(n_jobs=-1), param_grid=param_grid, cv=10)
-model = model.fit(X=x_train, y=y_train)
-print("Best parameters on training set: {0}".format(model.best_params_))
-print("Best cross-validated accuracy: {0}".format(model.best_score_))
-print("Accuracy on holdout test set: {0}".format(model.score(x_test, y_test)))
+extra_tree = GridSearchCV(ExtraTreesClassifier(n_jobs=-1), param_grid=param_grid, cv=10)
+extra_tree = extra_tree.fit(X=x_train, y=y_train)
+logging.info("Training End: Model ExtraTrees")
+logging.info("Extreme Random Tree Results:")
+logging.info("Best parameters on training set: {0}".format(extra_tree.best_params_))
+logging.info("Best cross-validated accuracy: {0}".format(extra_tree.best_score_))
+logging.info("Accuracy on holdout test set: {0}".format(extra_tree.score(x_test, y_test)))
+
+# Linear SVC
+param_grid = {}
+
+
 
 print("Predicting...")
 # Your trained model is now used to make predictions on the numerai_tournament_data
 # The model returns two columns: [probability of 0, probability of 1]
 # We are just interested in the probability that the target is 1.
-y_prediction = model.predict_proba(tournament)
+y_prediction = extra_tree.predict_proba(tournament)
 results = y_prediction[:, 1]
 results_df = pd.DataFrame(data={'probability': results})
 joined = pd.DataFrame(ids).join(results_df)
