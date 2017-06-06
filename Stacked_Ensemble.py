df165fadf2c160045466e9aa61e790e5a8eaeaf3
Datenbergwerker <spartan2@hotmail.de>
2017-06-06 12:37:21 +0200

    fully fixed stacked classifier, no error run, meta estimator param grid update

--- Stacked_Ensemble.py
+++ Stacked_Ensemble.py
@@ -50,8 +50,8 @@ logging.basicConfig(filename="numerai_logger.log",
 logging.info("Loading Data")
 training_data = pd.read_csv('numerai_training_data.csv', header=0)
 prediction_data = pd.read_csv('numerai_tournament_data.csv', header=0)
-training_data = training_data.sample(n=15)
-prediction_data = prediction_data.sample(n=15)
+# training_data = training_data.sample(n=1000)
+# prediction_data = prediction_data.sample(n=1000)
 features = [f for f in list(training_data) if "feature" in f]
 X = training_data[features]
 Y = training_data["target"]
@@ -66,7 +66,8 @@ models = {"ExtraTrees": [ExtraTreesClassifier(n_jobs=-1),
                                            ("logisticRegression", LogisticRegression(solver="sag", n_jobs=-1))]),
                                  {"logisticRegression__C": [0.1, 1, 10]}],
           "KNearestNeighbors": [KNeighborsClassifier(algorithm="kd_tree", n_jobs=-1),
-                                {"n_neighbors": range(5, 250, 5)}]}
+                                {"n_neighbors": range(5, 250, 5)}],
+          "MetaClassifier": {"n_estimators": [val for sublist in [[10, 25], list(range(50, 501, 50))] for val in sublist]}}
 
 skf = StratifiedKFold(n_splits=3)
 no_of_models = len(models.keys())
@@ -79,7 +80,7 @@ for j, (train, test) in enumerate(skf.split(X=X, y=Y)):
     x_test, y_test = X.iloc[test], Y.iloc[test]
     for i, model in enumerate(models.keys()):
         logging.info("Begin Training: {model}, Model {i} of {len}".format(model=model,
-                                                                          i=i+1,
+                                                                          i=i + 1,
                                                                           len=no_of_models))
         cur_model = GridSearchCV(models[model][0], param_grid=models[model][1], cv=5)
         cur_model.fit(X=x_train, y=y_train)
@@ -91,13 +92,13 @@ for j, (train, test) in enumerate(skf.split(X=X, y=Y)):
 # stacked classifier
 logging.info("Base model training complete. Starting meta classifier training.")
 skf = StratifiedKFold(n_splits=2)
-ensemble_tree = RandomForestClassifier(n_jobs=-1)
-meta_classifier = GridSearchCV(ensemble_tree, param_grid=models["ExtraTrees"])
+meta_classifier = GridSearchCV(RandomForestClassifier(n_jobs=-1), param_grid=models["MetaClassifier"], cv=5)
+meta_classifier.fit(X=predictions_train, y=Y)
 params = gen_param_dict(meta_classifier, x_test=predictions_train, y_test=Y)
 grid_search_report(meta_classifier, params=params)
 
-logging.info("Meta classifier training complete. Predicting tournament propabilities.")
-y_prediction = ensemble_tree.predict_proba(predictions_submission)[:, 1]
+logging.info("Meta classifier training complete. Predicting tournament probabilities.")
+y_prediction = meta_classifier.predict_proba(predictions_submission)[:, 1]
 logging.info("Writing predictions to predictions.csv")
 final = pd.DataFrame(ids, y_prediction)
 final.to_csv("predictions.csv", index=False)
